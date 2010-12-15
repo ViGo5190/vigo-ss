b395be805a83bb9250e1d95335d653061db257d9
Eric Bidelman <ericbidelman@html5rocks.com>
2010-12-14 16:30:24 -0800

    Merging

--- www.html5rocks.com/static/js/profiles.js
+++ www.html5rocks.com/static/js/profiles.js
@@ -1,8 +1,15 @@
 $(function() {
-  console.log("run ning");
-  $(".profile").click(function() {
-    console.log("clicked");
-    $(".profile").removeClass("active");
-    $(this).addClass("active");
+
+  $(".profile").click(function(ev) {
+    $(this).toggleClass("active");
+    $(".profile").not(this).removeClass("active");
+    return false;
   });
+
+  $(window).hashchange( function(){
+    $(".profile").removeClass("active");
+    $("[data-profile-id='"+location.hash+"']").addClass("active");
+  })
+  $(window).hashchange(); // run on page load
+
 });
