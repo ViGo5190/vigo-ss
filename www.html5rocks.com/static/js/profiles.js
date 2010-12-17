d93f1371138ee18aa1ef566a06a7d1ab079c6f06
Michael Mahemoff <mahemoff@html5rocks.com>
2010-12-17 15:02:01 +0000

    - when active, click profile again to de-activate - change hash handling using HTML5 history to support / (no #! if no-one active) - add lanyrd support

---
+++ www.html5rocks.com/static/js/profiles.js
@@ -0,0 +1,56 @@
+$(function() {
+  function updateHash(e) {
+    $activeProfile = $(".active");
+    if ($activeProfile.length) {
+      history.replaceState({}, document.title, '/profiles/#!/' + $activeProfile.attr("id"));
+    } else {
+      if (!!window.history) {
+        history.replaceState({}, document.title, '/profiles/');
+      } else {
+        location.hash = "/#!/"; // oh well, old browsers have to live with a #
+      }
+    }
+  }
+
+  window.slideMap = function(el) {
+    //$(el).toggleClass('active');
+  };
+
+  window.scrollToProfile = function(opt_profileID) {
+    var profileID = opt_profileID || null;
+    if (!profileID && location.hash.length) {
+      profileID = '#' + location.hash.split('#!/')[1];
+    }
+    if (profileID) {
+      $.scrollTo(profileID, 800, {offset: {top: -12}, onAfter: function() {
+        $(profileID).addClass("active");
+      }});
+    }
+  };
+
+  $(".profile").click(function(e) {
+    $(".profile").not(this).removeClass("active");
+    $(this).toggleClass("active");
+    updateHash(e);
+    //onHashChange('#' + $(this).attr('id'));
+    e.stopPropagation();
+  });
+
+  $(window).click(function(e) {
+    $(".profile").removeClass("active");
+    updateHash(e);
+  });
+
+  function onHashChange(profileID) {
+    $(".profile").removeClass("active");
+    window.scrollToProfile(profileID);
+  }
+
+  window.onhashchange = function(e) {
+    if (!location.hash.length) {
+      return;
+    }
+    onHashChange('#' + location.hash.split('/#!/')[1]);
+  };
+
+});
