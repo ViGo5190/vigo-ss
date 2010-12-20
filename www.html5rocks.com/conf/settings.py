2278b510aea315ee91c58fcfb2eb0ca0a8d4d2ae
Mike West <mkwst@google.com>
2010-12-20 11:56:38 +0100

    Starting on i18n project (Issue 359)

---
+++ www.html5rocks.com/conf/settings.py
@@ -0,0 +1,10 @@
+USE_I18N = True
+
+# Valid languages
+LANGUAGES = (
+    ('en', _('English')),
+    ('de_DE', _('German')),
+    ('zh_TW', _('Chinese')),
+)
+# This is a default language
+LANGUAGE_CODE = 'en'
